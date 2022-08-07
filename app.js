231300b32ec049855a5f66dd3e667ec60665bdf7
Elves00 <91095059+Elves00@users.noreply.github.com>
2022-08-08 11:38:48 +1200

    testing heroku

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // app.js
-const path = require('path');
+const path = require("path");
 const express = require("express");
 const connectDB = require("./config/db");
 var cors = require("cors");
@@ -9,8 +9,8 @@ const books = require("./routes/api/books");
 
 const app = express();
 
-
-const publicPath = path.join(__dirname, '..', 'public');
+//For connencting app to heroku
+const publicPath = path.join(__dirname, "..", "public");
 app.use(express.static(publicPath));
 
 // Connect Database
@@ -22,7 +22,12 @@ app.use(cors({ origin: true, credentials: true }));
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) => res.send("Hello world!"));
+// app.get("/", (req, res) => res.send("Hello world!"));
+
+//Ensure index html file is served in case of user requesting a resource currently not in public folder
+app.get("*", (req, res) => {
+  res.sendFile(path.join(publicPath, "index.html"));
+});
 
 // use Routes
 app.use("/api/books", books);
